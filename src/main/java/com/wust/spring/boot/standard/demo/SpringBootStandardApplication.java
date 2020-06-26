50589c010b592c4e866fc006c3bdf0869bd460ba
wustzdy <714403855@qq.com>
2020-06-27 00:42:58 +0800

    optimize project

--- src/main/java/com/wust/spring/boot/standard/demo/SpringBootStandardApplication.java
+++ src/main/java/com/wust/spring/boot/standard/demo/SpringBootStandardApplication.java
@@ -1,16 +1,20 @@
 package com.wust.spring.boot.standard.demo;
 
 
+import com.wust.spring.boot.standard.demo.mapper.UserMapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-//@MapperScan("com.wust.spring.boot.standard.demo.mapper")
+@MapperScan(value = "com.wust.spring.boot.standard.demo.mapper.*Mapper")
+@ComponentScan("com.wust.spring.boot.standard.demo.*")
 public class SpringBootStandardApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootStandardApplication.class, args);
--- src/main/java/com/wust/spring/boot/standard/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/wust/spring/boot/standard/demo/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.wust.spring.boot.standard.demo.service.impl;
 
+import com.sensetime.sensegear.iam.spec.contant.UserGenderType;
 import com.wust.spring.boot.standard.demo.entity.UserEntity;
 import com.wust.spring.boot.standard.demo.mapper.UserMapper;
 import com.wust.spring.boot.standard.demo.model.User;
@@ -7,7 +8,9 @@ import com.wust.spring.boot.standard.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 
+@SuppressWarnings("all")
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired(required = false)
@@ -17,8 +20,8 @@ public class UserServiceImpl implements UserService {
     public Integer createUser(User user) {
         UserEntity userEntity = new UserEntity();
         userEntity.setUserName(user.getUserName());
-        userEntity.setBirthday(user.getBirthday());
-        userEntity.setGender(user.getGender());
+        userEntity.setBirthday(new Date());
+        userEntity.setGender(UserGenderType.FEMALE);
 
         return userMapper.insert(userEntity);
     }
